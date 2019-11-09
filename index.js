313e87c48cda4ee0e2c147b2f85804bf0044a6da
johnflentri <jpflentri@gmail.com>
2019-11-09 13:20:14 +0100

    initial css layout, randomise button and extra questions added

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-let flashcards = [
+let flashcardsDutch = [
     { id: 1, question: 'good afternoon', answer: 'goedemiddag' },
     { id: 2, question: 'bicycle', answer: 'fiets' },
     { id: 3, question: 'vacuum cleaner', answer: 'stofzuiger' },
+    { id: 4, question: 'year', answer: 'jaar' },
+    { id: 5, question: 'today', answer: 'vandaag' },
+    { id: 6, question: 'tomorrow', answer: 'morgen' },
+    { id: 7, question: 'yesterday', answer: 'gisteren' },
+    { id: 8, question: 'use', answer: 'gebruiken' },
+    { id: 9, question: 'difficult', answer: 'moeilijk' },
+    { id: 10, question: 'easy', answer: 'gemakkelijk' },
 ]
 
-let randomCardNumber = Math.floor(Math.random() * flashcards.length)
+let randomCardNumberDutch = Math.floor(Math.random() * flashcardsDutch.length)
 
-console.log(randomCardNumber)
+function randomWordDutch() {
+    randomCardNumberDutch = Math.floor(Math.random() * flashcardsDutch.length)
+    document.getElementById("randomQuestionDutch").innerHTML = flashcardsDutch[randomCardNumberDutch].question;
+}
 

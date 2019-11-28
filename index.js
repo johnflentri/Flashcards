9b22958580e6d22f089d67674594e97d849f8d0a
johnflentri <jpflentri@gmail.com>
2019-11-28 11:35:08 +0100

    working through functions using arguments to require less code

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 let flashcardsDutch = [
     { id: 1, question: 'good afternoon', answer: 'goedemiddag' },
     { id: 2, question: 'bicycle', answer: 'fiets' },
-    // { id: 3, question: 'vacuum cleaner', answer: 'stofzuiger' },
+    { id: 3, question: 'vacuum cleaner', answer: 'stofzuiger' },
     { id: 4, question: 'year', answer: 'jaar' },
-    // { id: 5, question: 'today', answer: 'vandaag' },
-    // { id: 6, question: 'tomorrow', answer: 'morgen' },
-    // { id: 7, question: 'yesterday', answer: 'gisteren' },
-    // { id: 8, question: 'use', answer: 'gebruiken' },
-    // { id: 9, question: 'difficult', answer: 'moeilijk' },
-    // { id: 10, question: 'easy', answer: 'gemakkelijk' },
+    { id: 5, question: 'today', answer: 'vandaag' },
+    { id: 6, question: 'tomorrow', answer: 'morgen' },
+    { id: 7, question: 'yesterday', answer: 'gisteren' },
+    { id: 8, question: 'use', answer: 'gebruiken' },
+    { id: 9, question: 'difficult', answer: 'moeilijk' },
+    { id: 10, question: 'easy', answer: 'gemakkelijk' },
 ]
 
-// let flashcardsDutchNew = [
-//     { id: 1, question: 'weather', answer: 'weer' },
-//     { id: 2, question: 'cheers', answer: 'proost' },
-//     { id: 3, question: 'flower', answer: 'bloem' },
-//     { id: 4, question: 'lightning', answer: 'bliksem' },
-//     { id: 5, question: 'celebrate', answer: 'vieren' },
-//     { id: 6, question: 'prince', answer: 'prins' },
-//     { id: 7, question: 'pigeon', answer: 'duif' },
-//     { id: 8, question: 'groceries', answer: 'boodschappen' },
-//     { id: 9, question: 'sunset', answer: 'zonsondergang' },
-//     { id: 10, question: 'ice cream', answer: 'ijsje' },
-// ]
+let flashcardsDutchNew = [
+    { id: 1, question: 'weather', answer: 'weer' },
+    { id: 2, question: 'cheers', answer: 'proost' },
+    { id: 3, question: 'flower', answer: 'bloem' },
+    { id: 4, question: 'lightning', answer: 'bliksem' },
+    { id: 5, question: 'celebrate', answer: 'vieren' },
+    { id: 6, question: 'prince', answer: 'prins' },
+    { id: 7, question: 'pigeon', answer: 'duif' },
+    { id: 8, question: 'groceries', answer: 'boodschappen' },
+    { id: 9, question: 'sunset', answer: 'zonsondergang' },
+    { id: 10, question: 'ice cream', answer: 'ijsje' },
+]
 
 // let flashcardsGerman = [
 //     { id: 1, question: 'good afternoon', answer: 'guten tag' },
@@ -76,16 +76,16 @@ let flashcardsDutch = [
 //     { id: 10, question: 'ice cream', answer: 'es krim' },
 // ]
 
-// let maths1
-// let maths2
-// let mathsOperator = ["+", "-", "*", "/"];
+let maths1
+let maths2
+let mathsOperator = ["+", "-", "*", "/"];
 
-// let flashcardsMathsB = [
-//     { id: 1, question: ((maths1 = Math.floor(Math.random() * 89) + 11) + mathsOperator[0] + (maths2 = Math.floor(Math.random() * 89) + 11)), answer: eval(maths1 + mathsOperator[0] + maths2) },
-//     { id: 2, question: ((maths1 = Math.floor(Math.random() * 89) + 11) + mathsOperator[1] + (maths2 = Math.floor(Math.random() * 89) + 11)), answer: eval(maths1 + mathsOperator[1] + maths2) },
-//     { id: 3, question: ((maths1 = Math.floor(Math.random() * 8) + 2) + mathsOperator[2] + (maths2 = Math.floor(Math.random() * 89) + 11)), answer: eval(maths1 + mathsOperator[2] + maths2) },
-//     { id: 4, question: eval((maths1 = Math.floor(Math.random() * 8) + 2) + mathsOperator[2] + (maths2 = Math.floor(Math.random() * 8) + 2)) + mathsOperator[3] + maths2, answer: maths1 },
-// ]
+let flashcardsMathsB = [
+    { id: 1, question: ((maths1 = Math.floor(Math.random() * 89) + 11) + mathsOperator[0] + (maths2 = Math.floor(Math.random() * 89) + 11)), answer: eval(maths1 + mathsOperator[0] + maths2) },
+    { id: 2, question: ((maths1 = Math.floor(Math.random() * 89) + 11) + mathsOperator[1] + (maths2 = Math.floor(Math.random() * 89) + 11)), answer: eval(maths1 + mathsOperator[1] + maths2) },
+    { id: 3, question: ((maths1 = Math.floor(Math.random() * 8) + 2) + mathsOperator[2] + (maths2 = Math.floor(Math.random() * 89) + 11)), answer: eval(maths1 + mathsOperator[2] + maths2) },
+    { id: 4, question: eval((maths1 = Math.floor(Math.random() * 8) + 2) + mathsOperator[2] + (maths2 = Math.floor(Math.random() * 8) + 2)) + mathsOperator[3] + maths2, answer: maths1 },
+]
 
 // let flashcardsMathsI = [
 //     { id: 1, question: ((maths1 = Math.floor(Math.random() * 899) + 101) + mathsOperator[0] + (maths2 = Math.floor(Math.random() * 899) + 101)), answer: eval(maths1 + mathsOperator[0] + maths2) },
@@ -102,20 +102,6 @@ let flashcardsDutch = [
 // ]
 
 // let flashcardsCreate = []
-
-// function randomCard(questionSet) {
-//     return questionSet[Math.floor(Math.random() * questionSet.length)]
-// }
-
-// let randomCardNumberDutch = flashcardsDutch[Math.floor(Math.random() * flashcardsDutch.length)]
-
-// let randomCardNumberDutch = Math.floor(Math.random() * flashcardsDutch.length);
-// let randomCardNumberGerman = Math.floor(Math.random() * flashcardsGerman.length);
-// let randomCardNumberIndonesian = Math.floor(Math.random() * flashcardsIndonesian.length);
-// let randomCardNumberMathsB = Math.floor(Math.random() * flashcardsMathsB.length);
-// let randomCardNumberMathsI = Math.floor(Math.random() * flashcardsMathsI.length);
-// let randomCardNumberMathsE = Math.floor(Math.random() * flashcardsMathsI.length);
-// let randomCardNumberCreate = Math.floor(Math.random() * flashcardsCreate.length);
 
 function randomiseNow(quizType, questionSet, randomCardNumber) {
     if (questionSet.length >= 1) {
@@ -141,9 +127,10 @@ function randomWordDutch(){
 //     randomiseNow('Indonesian', flashcardsIndonesian)
 // }
 
-// function randomEqMathsB(){
-//     randomiseNow('MathsB', flashcardsMathsB)
-// }
+function randomEqMathsB(){
+    randomCardNumberMathsB = flashcardsMathsB[Math.floor(Math.random() * flashcardsMathsB.length)]
+    randomiseNow('MathsB', flashcardsMathsB, randomCardNumberMathsB)
+}
 
 // function randomEqMathsI(){
 //     randomiseNow('MathsI', flashcardsMathsI)
@@ -157,19 +144,19 @@ function randomWordDutch(){
 //     randomiseNow('Create', flashcardsCreate)
 // }
 
-// function getNew(questionSet, questionSetNew) {
-//     if (questionSetNew.length >= 1) {
-//         questionSet.push(questionSetNew[0])
-//         questionSetNew.shift(questionSetNew)
-//     } else {
-//         alert("You have added all the bonus questions!")
-//     }
-// }
+function getNewLanguage(questionSet, questionSetNew) {
+    if (questionSetNew.length >= 1) {
+        questionSet.push(questionSetNew[0])
+        questionSetNew.shift(questionSetNew)
+    } else {
+        alert("You have added all the bonus questions!")
+    }
+}
 
-// function getNewDutch() {
-//     getNew(flashcardsDutch, flashcardsDutchNew)
-//     updateTotalDutch();
-// }
+function getNewDutch() {
+    getNewLanguage(flashcardsDutch, flashcardsDutchNew)
+    // updateTotalDutch();
+}
 
 // function getNewGerman() {
 //     getNew(flashcardsGerman, flashcardsGermanNew)
@@ -180,6 +167,16 @@ function randomWordDutch(){
 //     getNew(flashcardsIndonesian, flashcardsIndonesianNew)
 //     updateTotalIndonesian();
 // }
+
+function getNewMaths(questionSet, questionSetNew) {
+    questionSet.push(questionSetNew)
+}
+
+function getNewMathsBAdd() {
+    flashcardsMathsBAdd = { id: flashcardsMathsB.length + 1, question: ((maths1 = Math.floor(Math.random() * 89) + 11) + mathsOperator[0] + (maths2 = Math.floor(Math.random() * 89) + 11)), answer: eval(maths1 + mathsOperator[0] + maths2) };
+    getNewMaths(flashcardsMathsB, flashcardsMathsBAdd)
+    // updateTotalMathsB();
+}
 
 // function getNewMathsBAdd() {
 //     flashcardsMathsB.push({ id: flashcardsMathsB.length + 1, question: ((maths1 = Math.floor(Math.random() * 89) + 11) + mathsOperator[0] + (maths2 = Math.floor(Math.random() * 89) + 11)), answer: eval(maths1 + mathsOperator[0] + maths2) });
@@ -275,15 +272,9 @@ function submitDutch() {
 //     }
 // }
 
-// function submitMathsB() {
-//     if (document.getElementById("randomQuestionMathsB").innerHTML == flashcardsMathsB[randomCardNumberMathsB].question && inputMathsB.value == flashcardsMathsB[randomCardNumberMathsB].answer) {
-//         document.getElementById("resultMathsBP").innerHTML = "Correct!";
-//     } else if (document.getElementById("randomQuestionMathsB").innerHTML == flashcardsMathsB[randomCardNumberMathsB].question && inputMathsB.value !== flashcardsMathsB[randomCardNumberMathsB].answer) {
-//         document.getElementById("resultMathsBP").innerHTML = "Incorrect.";
-//     } else {
-//         alert("You need to generate an equation first!")
-//     }
-// }
+function submitMathsB() {
+    submit('MathsB', randomCardNumberMathsB)
+}
 
 // function submitMathsI() {
 //     if (document.getElementById("randomQuestionMathsI").innerHTML == flashcardsMathsI[randomCardNumberMathsI].question && inputMathsI.value == flashcardsMathsI[randomCardNumberMathsI].answer) {
@@ -315,13 +306,17 @@ function submitDutch() {
 //     }
 // }
 
-// function showAnswerDutch() {
-//     if (document.getElementById("randomQuestionDutch").innerHTML == flashcardsDutch[randomCardNumberDutch].question) {
-//         document.getElementById("showAnswerDutchP").innerHTML = flashcardsDutch[randomCardNumberDutch].answer;
-//     } else {
-//         alert("You need to generate a word first!");
-//     }
-// }
+function showAnswer(quizType, randomCardNumber) {
+    if (document.getElementById(`randomQuestion${quizType}`).innerHTML == randomCardNumber.question) {
+        document.getElementById(`showAnswer${quizType}P`).innerHTML = randomCardNumber.answer;
+    } else {
+        alert("You need to generate a word first!");
+    }
+}
+
+function showAnswerDutch() {
+    showAnswer('Dutch', randomCardNumberDutch)
+}
 
 // function showAnswerGerman() {
 //     if (document.getElementById("randomQuestionGerman").innerHTML == flashcardsGerman[randomCardNumberGerman].question) {
@@ -338,6 +333,10 @@ function submitDutch() {
 //         alert("You need to generate a word first!");
 //     }
 // }
+
+function showAnswerMathsB() {
+    showAnswer('MathsB', randomCardNumberMathsB)
+}
 
 // function showAnswerMathsB() {
 //     if (document.getElementById("randomQuestionMathsB").innerHTML == flashcardsMathsB[randomCardNumberMathsB].question) {
